547500fe995cf31bef9e3a6b3c4598295c458842
ISMAIL <ismailmarkhi17@gmail.com>
2025-11-07 14:44:10 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-// import ApiLap from "./Comp/ApiLap";
-// import Status from "./Comp/Status";
-import ProductList from './Comp/ProductList';
+import TodoApp from './Comp/TodoApp';
 function App() {
 
   return (
     <>
-    <ProductList/>
+    <TodoApp/>
     </>
   )
 }
